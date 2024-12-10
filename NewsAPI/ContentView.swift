83baf3e867a0efa0f41752efdd79bfa76935a3c1
Neal Ahuja <nealahuja777@gmail.com>
2024-12-10 15:06:18 -0500

    added api call

--- NewsAPI/ContentView.swift
+++ NewsAPI/ContentView.swift
@@ -13,9 +13,17 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Goodbye, world!")
+            
+        }
+        task {
+            let url = URL(string: "https://newsapi.org/v2/everything?q=apple&from=2024-12-08&to=2024-12-08&sortBy=popularity&apiKey=fea0b318b4904bcaa01b5f166ad7e990")!
+
+               let (data, _) = try! await URLSession.shared.data(from: url)
+
+               print(String(decoding: data, as: UTF8.self))
         }
         .padding()
+       
     }
 }
 
